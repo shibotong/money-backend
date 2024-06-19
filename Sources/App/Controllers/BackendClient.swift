4deab6f4331fd20101cb2a04bd990bb7f88e5055
Shibo Tong <shibo0617@gmail.com>
2024-06-20 09:46:55 +1000

    init commit

--- Sources/App/Controllers/BackendClient.swift
+++ Sources/App/Controllers/BackendClient.swift
@@ -1,6 +1,6 @@
 //
 //  BackendClient.swift
-//  
+//
 //
 //  Created by Shibo Tong on 14/6/2024.
 //
@@ -8,32 +8,63 @@
 import Foundation
 import PostgresNIO
 
-class BackendClient {
+final class BackendClient: Sendable {
     
     static let shared = BackendClient()
     
     let client: PostgresClient
     
     init() {
+        // createdb mydb -p 3001 -U abcd -W abcd
         let config = PostgresClient.Configuration(
             host: "localhost",
             port: 5432,
-            username: "my",
-            password: "",
-            database: "mydb",
+            username: "money_app",
+            password: "money_app",
+            database: "money-app",
             tls: .disable
         )
         
         client = PostgresClient(configuration: config)
+        
+        Task {
+            await client.run()
+        }
     }
     
-    func startClient() async {
-        await withTaskGroup(of: Void.self) { taskGroup in
-            taskGroup.addTask { [weak self] in
-                await self?.client.run()
+    func queryUser() async -> String {
+        do {
+            let rows = try await client.query("SELECT rolname FROM pg_roles")
+            var result = ""
+            for try await row in rows {
+                // do something with the row
+                result += row.description
             }
-            
-            taskGroup.cancelAll()
+            return result
+        } catch {
+            return error.localizedDescription
         }
+    }
+    
+    func queryUserTables() async -> String {
+        do {
+            let rows = try await client.query("SELECT user_name, password FROM moneyuser")
+            var result = ""
+            for try await row in rows {
+                // do something with the row
+                result += row.description
+            }
+            return result
+        } catch {
+            return error.localizedDescription
+        }
+    }
+    
+    func createUser(name: String, password: String) async throws -> String {
+        let queryString = "INSERT INTO MONEYUSER (user_name, password) VALUES ('\(name)', '\(password)')"
+        print(queryString)
+        let query = PostgresQuery(stringLiteral: queryString)
+        try await client.query(query)
+        return "Insert user success"
     }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,21 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("postgresql") { req async -> String in
+        return await BackendClient.shared.queryUser()
+    }
+    
+    app.get("users") { req async -> String in
+        return await BackendClient.shared.queryUserTables()
+    }
+    
+    app.get("create") { req async -> String in
+        do {
+            let result = try await BackendClient.shared.createUser(name: "testuser", password: "testpass")
+            return result
+        } catch {
+            return error.localizedDescription
+        }
+    }
 }
