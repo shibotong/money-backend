79ed1fb2f1295d576ad0d4b8198e5d2a58f70d7e
Shibo Tong <shibo0617@gmail.com>
2024-07-06 22:58:24 +1000

    add category table

--- Sources/App/Model/Category.swift
+++ Sources/App/Model/Category.swift
@@ -8,28 +8,25 @@
 import Foundation
 import Vapor
 import Fluent
-//
-//final class Category: Model, Content {
-//    
-//    static let schema = "users"
-//    
-//    @ID(key: .id)
-//    var id: UUID?
-//    
-//    @Field(key: "category_name")
-//    var name: String
-//    
-//    @Field(key: "user_id")
-//    var userid: UUID
-//    
-//    init() {}
-//    
-//    init(id: UUID? = nil, username: String, password: String, currency: String = "USD", admin: Bool = false, deleted: Bool = false) {
-//        self.id = id
-//        self.username = username
-//        self.password = password
-//        self.currency = currency
-//        self.admin = admin
-//        self.deleted = deleted
-//    }
-//}
+
+final class Category: Model, Content {
+    
+    static let schema = "category"
+    
+    @ID(key: .id)
+    var id: UUID?
+    
+    @Field(key: "category_name")
+    var name: String
+    
+    @Field(key: "user_id")
+    var userid: UUID
+    
+    init() {}
+    
+    init(id: UUID? = nil, name: String, userid: UUID) {
+        self.id = id
+        self.name = name
+        self.userid = userid
+    }
+}
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -23,13 +23,22 @@ public func configure(_ app: Application) async throws {
 
 public func initService(_ postgres: PostgresDatabase) async throws {
     _ = try await postgres.simpleQuery("""
-                            CREATE TABLE IF NOT EXISTS users (
-                                id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-                                username VARCHAR(100) NOT NULL,
-                                password VARCHAR(100) NOT NULL,
-                                currency VARCHAR(3),
-                                admin BOOLEAN NOT NULL DEFAULT false,
-                                deleted_at TIMESTAMP
-                            )
-                            """).get()
+                                        CREATE TABLE IF NOT EXISTS users (
+                                            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+                                            username VARCHAR(100) NOT NULL,
+                                            password VARCHAR(100) NOT NULL,
+                                            currency VARCHAR(3),
+                                            admin BOOLEAN NOT NULL DEFAULT false,
+                                            deleted_at TIMESTAMP
+                                        )
+                                        """).get()
+    
+    _ = try await postgres.simpleQuery("""
+                                        CREATE TABLE category (
+                                            id SERIAL PRIMARY KEY,
+                                            name VARCHAR(255),
+                                            userid INT REFERENCES users(id),
+                                            deleted_at TIMESTAMP
+                                        )
+                                        """).get()
 }
